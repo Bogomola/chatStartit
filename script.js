3425e06af4548b776fe03bb26b3928c9633755e6
Bogomola <90862767+Bogomola@users.noreply.github.com>
2022-03-18 11:38:25 +0200

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,10 @@ function sutitZinu()
     console.log('sutitZinu() darbojas');
     zinas.innerHTML=zinas.innerHTML+ '<br/>'+zina.value;
 }
+async function ieladetChataZinas()
+{
+let datiNoServera = await fetch("chatazinas.txt");
+let dati = await  datiNoServera.text();
+//console.log(dati);
+zinas.innerHTML=dati;
+}
